4dce5b3672ebeda1eb8f6ca21242267c99c08eed
Rajesham Gajjela <rajeshamdev@gmail.com>
2017-11-14 20:33:54 -0600

    Producer/Consumer demonstration program.

--- interviewFAQ/producerConsumer.cpp
+++ interviewFAQ/producerConsumer.cpp
@@ -1,63 +1,80 @@
+/**
+ * Demonstration of producer/consumer problem with mutex/condvar.
+ * g++ -g -std=c++11 producerConsumer.cpp -lpthread
+ */
+
 #include <iostream>
-#include <thread>
+#include <mutex>
 #include <queue>
+#include <condition_variable>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
-class BoundQueue {
+class boundQueue {
     private:
-        mutex queueLock;
-        condition_variable queueCv;
-        queue <int> sharedQueue;
-        int maxQueueSize;
-        void producer(int);
-        void consumer(void);
+        mutex mu;
+        condition_variable cv_qfull; // wait when queue is full
+        condition_variable cv_qempty; // wait when the queus is empty
+        queue<int> bq;
+        int qsize;
     public:
-        BoundQueue(int size) : maxQueueSize(size) {}
-        void run(void);
+        void put(int data);
+        int get(void);
+        int size(void) {return qsize;}
+        boundQueue(int size) : qsize(size) {}
 };
 
-void
-BoundQueue::producer(int n)
+void boundQueue::put(int data)
 {
-    int count = 10;
-    while (count--) {
-        unique_lock<mutex> ql(queueLock);
-        queueCv.wait(ql, [this]{return sharedQueue.size() < maxQueueSize;});
-        sharedQueue.push(n++);
-        ql.unlock();
-        queueCv.notify_all();
+    unique_lock<mutex> plock(mu);
+    cv_qfull.wait(plock, [this](){return bq.size() < qsize;});
+    bq.push(data);
+    cout << "producer, pushed data: " << data << "\n";
+    plock.unlock();
+    cv_qempty.notify_one();
+}
+
+
+int boundQueue::get(void)
+{
+    unique_lock<mutex> conlock(mu);
+    cv_qempty.wait(conlock, [this](){return bq.size() > 0;});
+    int data = bq.front();
+    bq.pop();
+    cout << "consumer, poped data: " << data << "\n";
+    conlock.unlock();
+    cv_qfull.notify_one();
+    return data;
+    
+}
+
+void producer(boundQueue &bq)
+{
+    int i = 0;
+    while (i < bq.size()) {
+        bq.put(i);
+        this_thread::sleep_for(chrono::milliseconds(100));
+        ++i;
+    }
+} 
+
+void consumer(boundQueue &bq)
+{
+    int i = 0;
+    while (i < bq.size()) {
+        int data = bq.get();
+        this_thread::sleep_for(chrono::milliseconds(250));
+        ++i;
     }
 }
 
-void
-BoundQueue::consumer(void)
+int main(void)
 {
-    int count = 10;
-    while (count--) {
-        unique_lock<mutex> ql(queueLock);
-        queueCv.wait(ql, [this]{return !sharedQueue.empty();});
-        int readval = sharedQueue.front();
-        cout << "Popped : " << readval << "\n";
-        sharedQueue.pop();
-        ql.unlock();
-        queueCv.notify_all();
-    }
-}
-
-void
-BoundQueue::run(void)
-{
-    thread t1(&BoundQueue::producer, this, 10);
-    thread t2(&BoundQueue::consumer, this);
+    boundQueue bq(10);
+    thread t1(producer, ref(bq)); 
+    thread t2(consumer, ref(bq)); 
     t1.join();
     t2.join();
-}
-
-int
-main(void)
-{
-    BoundQueue boundQueue(5);
-    boundQueue.run();
-    return 0;
 }
