993026c42d1b6bc5d9037b6e1aac68e2dcbfaf70
Rajesham Gajjela <rajeshamdev@gmail.com>
2017-11-15 23:41:58 -0600

    Reverse a string.

---
+++ interviewFAQ/reverseString.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* Walks through entire string. Slightly inefficient */
+string strReverse1(string str)
+{
+    string name;
+    int len = str.size()-1;
+    while (len >= 0) {
+        name += str[len--];
+    }
+    return name;
+}
+
+/* Walks through about half size of the string. Better compared to above. */
+void strReverse2(string &str)
+{
+    int len = str.size()-1;
+    int i = 0;
+    for (int i = 0; i < len; i++, --len) {
+         char a = str[i];
+         str[i] = str[len];
+         str[len] = a;
+    }
+}
+
+int main(void)
+{
+    string name = "Veritas interview";
+    string name1 = strReverse1(name);
+    cout << "name = " + name1 + "\n";
+
+    strReverse2(name1);
+    cout << "name = " + name1 + "\n";
+}
