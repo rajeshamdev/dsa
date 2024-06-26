352119972fd640c152b44666571c2b5c56fc9a96
rajeshamdev <rajeshamdev@gmail.com>
2024-06-25 23:35:26 -0500

    Queue unit tests.

--- godsa/queue/queue_test.go
+++ godsa/queue/queue_test.go
@@ -38,9 +38,9 @@ func TestDequeue(t *testing.T) {
 }
 
 func TestIsEmpty(t *testing.T) {
-	q := Init()
 
-	if !q.IsEmpty() {
+	q := Init()
+	if !(q.IsEmpty()) {
 		t.Error("queue is expected to be empty")
 	}
 
@@ -58,7 +58,51 @@ func TestIsEmpty(t *testing.T) {
 		q.Dequeue()
 	}
 
-	if !q.IsEmpty() {
+	if !(q.IsEmpty()) {
 		t.Error("queue is expected to be empty")
+	}
+}
+
+func TestFront(t *testing.T) {
+
+	q := Init()
+	if _, err := q.Front(); err == nil {
+		t.Error("this should have been error as there is nothing in queue\n")
+	}
+
+	items := []int{2, 3, 11, 5, 9, 7, 10, 21, 25, 1}
+	for _, item := range items {
+		q.Enqueue(item)
+	}
+
+	item, err := q.Front()
+	if err != nil {
+		t.Errorf("Front returned: %v\n", err)
+	}
+	if item != items[0] {
+		t.Errorf("Front error. expected = %v, returned = %v\n", items[0], item)
+	}
+
+	// dequeue all items
+	for i := 1; i <= len(items); i++ {
+		q.Dequeue()
+	}
+
+	if _, err = q.Front(); err == nil {
+		t.Error("this should have been error as there is nothing in queue\n")
+	}
+}
+
+func TestSize(t *testing.T) {
+
+	q := Init()
+	items := []int{2, 3, 11, 5, 9, 7, 10, 21, 25, 1}
+	for _, item := range items {
+		q.Enqueue(item)
+	}
+
+	size := q.Size()
+	if size != len(items) {
+		t.Errorf("queue size expected = %v, actual = %v\n", len(items), size)
 	}
 }
